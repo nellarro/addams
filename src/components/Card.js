64ccc4b15d091685a2d9b9b4b8a9506015c96fa5
Jenell Pizarro <nellarro@gmail.com>
2016-09-03 15:24:45 -0400

    all pictures in

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,11 @@ class Card extends React.Component {
 
   render () {
     const direction = this.props.up ? 'up' : 'down'
-    return <div onClick={this.handleClick} className={`card ${direction}`}>{this.props.value}</div>
     switch (direction) {
-      case 'down': return <img src='..images/dead-roses.jpg' />
-    }
+      case 'up': return <img src={this.props.value} className={`card ${direction}`} />
+      case 'down': return <img src='https://s-media-cache-ak0.pinimg.com/236x/9a/3a/e2/9a3ae2e10d5b196b0a228ca00f8219fc.jpg' onClick={this.handleClick} className={`card ${direction}`} />
+      default: return <img src='https://s-media-cache-ak0.pinimg.com/236x/9a/3a/e2/9a3ae2e10d5b196b0a228ca00f8219fc.jpg' onClick={this.handleClick} className={`card ${direction}`} />
+  }
   }
 }
 
