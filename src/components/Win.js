03538da3165811ce1a74397da3669478a52fdfb6
Jenell Pizarro <nellarro@gmail.com>
2016-09-06 15:34:06 -0400

    there's a squirrel in my atom so...

---
+++ src/components/Win.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Win extends React.Component {
+
+  render () {
+    <div className='WinStatement'>
+      <h2> Normal is an illusion. </h2>
+      <h4> What is normal for the spider is chaos for the fly.</h4>
+    </div>
+  }
+}
+
+export default Win
